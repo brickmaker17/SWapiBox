246d8c31cd4e1e98de18d5de887cb44a79225a98
Justin Stewart <Justinstewart3313@gmail.com>
2018-10-09 08:52:56 -0600

    Setup Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
+
 import './App.css';
+import IntroScreen from '../IntroScreen/IntroScreen.js'
+import CardContainer from './CardContainer/CardContainer.js'
+
 
 class App extends Component {
+
+
+
+  componentDidMount(){
+
+  }
+
+
   render() {
     return (
       <div className="App">
+      <IntroScreen/>
+      <CardContainer />
+
       </div>
     );
   }
