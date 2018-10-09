8d9979d2225159e0879b7f4b7a34fd3175939d70
Justin Stewart <Justinstewart3313@gmail.com>
2018-10-09 10:24:10 -0600

    Set up helper file

--- src/App.js
+++ src/App.js
@@ -6,18 +6,28 @@ import CardContainer from './CardContainer/CardContainer.js'
 
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      people: [],
+      vehicles: [],
+      planets: [],
+      films: []
+    }
+  }
 
 
 
   componentDidMount(){
-
+    
   }
 
 
   render() {
+    const { films } = this.state
     return (
       <div className="App">
-      <IntroScreen/>
+      <IntroScreen films={ films }/>
       <CardContainer />
 
       </div>
