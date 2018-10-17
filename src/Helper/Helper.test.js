6c528c82f3c4193412d15e55289e25a6fb57c12b
Alex Bruce <brucealmightyalex@yahoo.com>
2018-10-17 15:14:44 -0600

    Add initial Helper test functionality

---
+++ src/Helper/Helper.test.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { configure, shallow } from 'enzyme';
+import App from '../App'
+
+import Helper from './Helper';
+import { promises } from 'fs';
+
+const getData = require('./Helper').getData;
+
+describe('Helper', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<App />)
+  })
+
+  it('getData should call fetch with correct param', async () => {
+    const expected = 'https://swapi.co/api/';
+
+    window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+      status: 200
+    }))
+    console.log(Helper)
+    getData()
+    await expect(window.fetch).toHaveBeenCalledWith(expected)
+  })
+
+  it('getFilms gets called with the correct params', () => {
+    
+  })
+
+  it('getFilms calls fetch with the proper params', () => {
+  })
+
+  it('should call getPeople', () => {
+  })
+
+  it('Should call fetchPerson', () => {
+  })
+
+  it('fetchPerson should call fetch with correct params', () => {
+  })
+
+  it('fetchPerson should call getSpecies', () => {
+  })
+
+  it('fetchPerson should call getHomeworld', () => {
+  })
+
+  it('fetchPerson should call getPopulation', () => {
+  })
+
+  it('should call getPlanets', () => {
+  })
+
+  it('getPlanets should call fetch with the correct params', () => {
+  })
+
+})
